fd3f0dc05f120befe52a124bcac831b9e2d79035
Noubar Topalian <topalian.noubar@gmail.com>
2020-03-12 17:13:01 +0100

    Chart and picking

--- circle.js
+++ circle.js
@@ -1,190 +1,132 @@
-let canvas = document.getElementById("canvas");
-let ctx = canvas.getContext("2d");
+// let canvas = document.getElementById("canvo");
+// let ctx = canvas.getContext("2d");
 
- // ****************************
-    // draw circle
-    // ****************************
-    //Y-Axis
-    ctx.beginPath()
-    ctx.moveTo(150,0)
-    ctx.lineTo(150,500)
-    ctx.stroke()
-    //X-Axis
-    ctx.moveTo(0,150)
-    ctx.lineTo(500,150)
-    ctx.stroke()
-    ctx.closePath()
-    
-    class Item{
-        //50 as radius
-       constructor(radius,startAngle,endAngle,color){
-        //50 as radius
-        this.r=radius
-        this.x=150
-        this.y=150
-        this.startAngle=startAngle
-        this.endAngle=endAngle
-        this.antiClockwise=true
-        this.color=color
-       }
-       drawPickedItem(){
-        ctx.beginPath();
-    // ctx.arc(x, y, radius, startAngle, endAngle)
-        ctx.arc(this.x, this.y, this.r, this.startAngle, this.endAngle,this.antiClockwise);
-        ctx.lineWidth = 20;
-        ctx.strokeStyle = this.color; // !
-        ctx.stroke();
-        ctx.closePath();
-       }
-
-
-    }
-    //To draw 1/4 of a circle in the top right side
-    //Which is MONEY! 
-    let startAngleMoney=0
-    let endAngleMoney=3*Math.PI/2
-    let colorMoney="green"
-    //The radius will increment only by 20
-    //First Item picked - itemMoney
-    let itemMoney=new Item(50,startAngleMoney,endAngleMoney,colorMoney)
-    itemMoney.drawPickedItem()
-    //Second Item picked
-    let item2Money=new Item(70,startAngleMoney,endAngleMoney,colorMoney)
-    item2Money.drawPickedItem()
-    //Third Item picked
-    let item3Money=new Item(90,startAngleMoney,endAngleMoney,colorMoney)
-    item3Money.drawPickedItem()
-    //Fourth Item picked
-    let item4Money=new Item(110,startAngleMoney,endAngleMoney,colorMoney)
-    item4Money.drawPickedItem()
-
-    //To draw 1/4 of a circle in the top left side
-    //Which is LOVE!
-    let startAngleLove=3*Math.PI/2
-    let endAngleLove=Math.PI
-    let colorLove="red"
-    //First Item picked
-    let itemLove=new Item(50,startAngleLove,endAngleLove,colorLove)
-    itemLove.drawPickedItem()
-    //Second Item picked
-    let itemLove2=new Item(70,startAngleLove,endAngleLove,colorLove)
-    itemLove2.drawPickedItem()
-    //Third Item picked
-    let itemLove3=new Item(90,startAngleLove,endAngleLove,colorLove)
-    itemLove3.drawPickedItem()
-    //Fourth Item picked
-    let itemLove4=new Item(110,startAngleLove,endAngleLove,colorLove)
-    itemLove4.drawPickedItem()
-
-    //To draw 1/4 of a circle in the bottom left side
-    //Which is HEALTH!
-    let startAngleHealth=Math.PI
-    let endAngleHealth=Math.PI/2
-    let colorHealth="blue"
-    //First Item picked
-    let itemHealth=new Item(50,startAngleHealth,endAngleHealth,colorHealth)
-    itemHealth.drawPickedItem()
-    //Second Item picked
-    let item2Health=new Item(70,startAngleHealth,endAngleHealth,colorHealth)
-    item2Health.drawPickedItem()
-    //Third Item picked
-    let item3Health=new Item(90,startAngleHealth,endAngleHealth,colorHealth)
-    item3Health.drawPickedItem()
-    //Third Item picked
-    let item4Health=new Item(110,startAngleHealth,endAngleHealth,colorHealth)
-    item4Health.drawPickedItem()
-
-    //To draw 1/4 of a circle in the bottom left side
-    //Which is Leisure!
-    let startAngleLeisure=Math.PI/2
-    let endAngleLeisure=0
-    let colorLeisure="orange"
-    //First Item picked
-    let itemLeisure=new Item(50,startAngleLeisure,endAngleLeisure,colorLeisure)
-    itemLeisure.drawPickedItem()
-    //Second Item picked
-    let item2Leisure=new Item(70,startAngleLeisure,endAngleLeisure,colorLeisure)
-    item2Leisure.drawPickedItem()
-    //Third Item picked
-    let item3Leisure=new Item(90,startAngleLeisure,endAngleLeisure,colorLeisure)
-    item3Leisure.drawPickedItem()
-    //Third Item picked
-    let item4Leisure=new Item(110,startAngleLeisure,endAngleLeisure,colorLeisure)
-    item4Leisure.drawPickedItem()
-
+// ****************************
+// draw circle
+// ****************************
+let drawMiddleAndAxis = () => {
     //Center of the circle
     ctx.beginPath();
-    ctx.arc(150, 150, 30, 0, Math.PI * 2);
+    ctx.arc(60, 60, 12, 0, Math.PI * 2);
     ctx.fillStyle = "black"; // !
-    // fills the inner circle with red color
+    // fills the inner circle with black color
     ctx.fill();
-    ctx.font = "18px serif";
-    ctx.fillStyle = "black";
-    ctx.fillText("Score ", 230, 255); //+points
+    //Y-Axis
+    ctx.beginPath();
+    ctx.strokeStyle="black"
+    ctx.lineWidth=2
+    //ctx.fillStyle = "black";
+    ctx.moveTo(60, 0);
+    ctx.lineTo(60, 120);
+    ctx.stroke();
+    //X-Axis
+    ctx.moveTo(0, 60);
+    ctx.lineTo(120, 60);
+    ctx.stroke();
     ctx.closePath();
+    }
+
+class Item {
+  //40 as radius
+  constructor(radius, startAngle, endAngle, color) {
+    //40 as radius
+    this.r = radius;
+    this.x = 60;
+    this.y = 60;
+    this.startAngle = startAngle;
+    this.endAngle = endAngle;
+    this.antiClockwise = true;
+    this.color = color;
+  }
+  drawPickedItem() {
+    //console.log("hello");
+    ctx.beginPath();
+    // ctx.arc(x, y, radius, startAngle, endAngle)
+    ctx.arc(
+      this.x,
+      this.y,
+      this.r,
+      this.startAngle,
+      this.endAngle,
+      this.antiClockwise
+    );
+    ctx.lineWidth = 10;
+    ctx.strokeStyle = this.color; // !
+    ctx.stroke();
+    ctx.closePath();
+  }
+}
+//console.log(cheese)
+//To draw 1/4 of a circle in the top right side
+//Which is MONEY!
 
 
+let drawItemMoney=(score)=>{
+    let startAngleMoney = 0;
+    let endAngleMoney = (3 * Math.PI) / 2;
+    let colorMoney = "green";
+    let radius=20
+    for(let i=0;i<score;i++){
+        let item = new Item(radius, startAngleMoney, endAngleMoney, colorMoney)
+        radius+=10
+        //We only need to catch the index in order to draw score!
+        item.drawPickedItem()
+    }
+}
 
-    //To draw 1/4 of a circle
+//To draw 1/4 of a circle in the top left side
+//Which is LOVE!
 
-    // //Top-Right circle
-    // ctx.beginPath();
-    // // ctx.arc(x, y, radius, startAngle, endAngle)
-    // ctx.arc(150, 150, 50, 0, 3*Math.PI / 2,true);
-    // ctx.lineWidth = 20;
-    // ctx.strokeStyle = "green"; // !
-    // ctx.stroke();
-    // ctx.closePath();
-    // //Second half circle
-    // ctx.beginPath();
-    // ctx.arc(150, 150, 70, 0, 3*Math.PI / 2,true);
-    // ctx.lineWidth = 20;
-    // ctx.strokeStyle = "green"; 
-    // ctx.stroke();
-    // ctx.closePath();
+let drawItemLove=(score)=>{
+    let startAngleLove = (3 * Math.PI) / 2;
+    let endAngleLove = Math.PI;
+    let colorLove = "red";
+    let radius=20
+    for(let i=0;i<score;i++){
+        let item=new Item(radius, startAngleLove, endAngleLove, colorLove)
+        radius+=10
+        //We only need to catch the index in order to draw score!
+        item.drawPickedItem()
+    }
+}
 
-    // //Top-Left circle
-    // ctx.beginPath();
-    // ctx.arc(150, 150, 50, 3*Math.PI/2,Math.PI,true);
-    // ctx.lineWidth = 20;
-    // ctx.strokeStyle = "red"; // !
-    // ctx.stroke();
-    // ctx.closePath();
-    // ctx.beginPath()
-    // ctx.arc(150, 150, 70, 3*Math.PI/2,Math.PI,true);
-    // ctx.lineWidth = 20;
-    // ctx.strokeStyle = "red"; // !
-    // ctx.stroke();
-    // ctx.closePath();
+//To draw 1/4 of a circle in the bottom left side
+//Which is HEALTH!
 
-    // //Bottom-Left circle
-    // ctx.beginPath()
-    // ctx.arc(150, 150, 50, Math.PI,Math.PI/2,true);
-    // ctx.lineWidth = 20;
-    // ctx.strokeStyle = "blue";
-    // ctx.stroke();
-    // ctx.closePath();
-    // ctx.beginPath()
-    // ctx.arc(150, 150, 70, Math.PI,Math.PI/2,true);
-    // ctx.lineWidth = 20;
-    // ctx.strokeStyle = "blue";
-    // ctx.stroke();
-    // ctx.closePath();
-
-    // //Bottom-Right circle
-    // ctx.beginPath()
-    // ctx.arc(150, 150, 50, Math.PI/2,0,true);
-    // ctx.lineWidth = 20;
-    // ctx.strokeStyle = "yellow";
-    // ctx.stroke();
-    // ctx.closePath();
-    // ctx.beginPath()
-    // ctx.arc(150, 150, 70, Math.PI/2,0,true);
-    // ctx.lineWidth = 20;
-    // ctx.strokeStyle = "yellow";
-    // ctx.stroke();
-    // ctx.closePath();
-
-
-
+let drawItemHealth=(score)=>{
+    let startAngleHealth = Math.PI;
+    let endAngleHealth = Math.PI / 2;
+    let colorHealth = "blue";
     
+    let radius=20
+    for(let i=0;i<score;i++){
+        let item=new Item(radius, startAngleHealth, endAngleHealth, colorHealth)
+        radius+=10
+        //We only need to catch the index in order to draw score!
+        item.drawPickedItem()
+    }
+}
+
+//To draw 1/4 of a circle in the bottom left side
+//Which is Leisure!
+
+let drawItemLeisure=(score)=>{
+    let startAngleLeisure = Math.PI / 2;
+    let endAngleLeisure = 0;
+    let colorLeisure = "orange";
+    
+    let radius=20
+    for(let i=0;i<score;i++){
+        let item=new Item(radius, startAngleLeisure, endAngleLeisure, colorLeisure)
+        radius+=10
+        //We only need to catch the index in order to draw score!
+        item.drawPickedItem()
+    }
+}
+
+
+
+// ctx.font = "18px serif";
+// ctx.fillStyle = "black";
+// ctx.fillText("Score ", 220, 255); //+points
+// ctx.closePath();
--- secondLayout.js
+++ secondLayout.js
@@ -1,275 +1,300 @@
-window.onload = () => {
-    document.getElementById("start-button").onclick = () => {
-        startGame();
-    };
-    // Getting the canvas from html page
-    let ctx = document.getElementById("canvas").getContext("2d");
-    // For ending the game
-    let runningGame = true;
-    // Create car objectfksdjf
-    let player = {
-        x: 220,
-        y: 400,
-        width: 50,
-        height: 50,
-        img: new Image(),
-        rightPressed: function () {
-            if (this.x < 450 - this.width) {
-                this.x += 15
-            }
-        },
-        leftPressed: function () {
-            if (this.x >= 50) {
-                this.x -= 15;
-            }
-        },
-        upPressed: function () {
-            if (this.y >= 50) {
-                this.y -= 15
-            }
-        },
-        downPressed: function () {
-            if (this.y <= 400) {
-                this.y += 15
-            }
-        },
-        update: function () {
-            // ctx.fillRect(this.x, this.y, this.width, this.height);
-            // this.img.src = "car.png";
-            // ctx.drawImage(roadImg, 0, 0, canvas.width, canvas.height);
-            ctx.fillStyle = 'black'
-            ctx.fillRect(this.x, this.y, this.width, this.height);
-        },
-        left: function () {
-            return this.x;
-        },
-        top: function () {
-            return this.y;
-        },
-        right: function () {
-            return this.x + this.width;
-        },
-        bottom: function () {
-            return this.y + this.height;
-        },
-        crash: function (obstacle) {
-            return !(
-                this.bottom() < obstacle.top() ||
-                this.top() > obstacle.bottom() ||
-                this.right() < obstacle.left() ||
-                this.left() > obstacle.right()
-            );
-        },
-        // win: function (winObj) {
-        //     return (
-        //         this.bottom() < winObj.top() ||
-        //         this.top() > winObj.bottom() ||
-        //         this.right() < winObj.left() ||
-        //         this.left() > winObj.right()
-        //     );
-        // }
-    };
-    // Create obstacles
-    class obstacle {
-        constructor(posX) {
-            this.x = posX;
-            this.y = 0;
-            this.width = 20;
-            this.height = 20;
-            this.color = 'green';
-            this.speedY = 2;
-        }
-        update() {
-            ctx.fillStyle = this.color;
-            ctx.fillRect(this.x, this.y, this.width, this.height);
-            // this.img.src = "car.png";
-            // ctx.drawImage(roadImg, 0, 0, canvas.width, canvas.height);
-            this.y += 2;
-        }
-        top() {
-            return this.y;
-        }
-        left() {
-            return this.x;
-        }
-        bottom() {
-            return this.y + this.height;
-        }
-        right() {
-            return this.x + this.width;
-        }
-        clear() {
-            let prevCoords = {};
-            // prevCoords.x = this.x;
-            // console.log("outPut: obstacle -> clear -> prevCoords.x", prevCoords.x)
-            // prevCoords.y = this.y - 2;
-            // console.log("outPut: obstacle -> clear -> prevCoords.y", prevCoords.y)
-            // lineWidth:fix
-            // ctx.clearRect(this.x, this.y + 2, this.width, this.height)
-            ctx.clearRect(0, 0, canvas.width, canvas.height);;
-        };
-    }
-
-    // class objectMoney extends object {
-    //     constructor(posX) {
-    //         super(posX)
-    //         this.y = 0;
-    //         this.width = 20;
-    //         this.height = 20;
-    //         this.color = 'green';
-    //         this.speedY = 2;
-    //         this.img = new Image()
-    //     }
-    // }
-    // class objectHealth extends object {
-    //     constructor(posX) {
-    //         super(posX)
-    //         this.y = 0;
-    //         this.width = 20;
-    //         this.height = 20;
-    //         this.color = 'blue';
-    //         this.speedY = 2;
-    //         this.img = new Image()
-    //     }
-    // }
-    // class objectLove extends object {
-    //     constructor(posX) {
-    //         super(posX)
-    //         this.y = 0;
-    //         this.width = 20;
-    //         this.height = 20;
-    //         this.color = 'red';
-    //         this.speedY = 2;
-    //         this.img = new Image()
-    //     }
-    // }
-    // class objectLeisure extends object {
-    //     constructor(posX) {
-    //         super(posX)
-    //         this.y = 0;
-    //         this.width = 20;
-    //         this.height = 20;
-    //         this.color = 'yellow';
-    //         this.speedY = 2;
-    //         this.img = new Image()
-    //     }
-    // }
-
-    // class winObject {
-    //     constructor(posX) {
-    //         this.x = posX;
-    //         this.y = 0;
-    //         this.width = 100;
-    //         this.height = 50;
-    //         this.color = 'blue';
-    //         this.speedY = 2;
-    //     }
-    //     update() {
-    //         ctx.fillStyle = this.color;
-    //         ctx.fillRect(this.x, this.y, this.width, this.height);
-    //         this.y += 2;
-    //     }
-    //     top() {
-    //         return this.y - 8;
-    //     }
-    //     left() {
-    //         return this.x - 8;
-    //     }
-    //     bottom() {
-    //         return this.y + this.height - 8;
-    //     }
-    //     right() {
-    //         return this.x + this.width - 8;
-    //     }
-    // }
-
-
-    let counter = 0;
-    let obstaclesArr = [];
-
-    // let obstaclesMoneyArr = [];
-    // let obstaclesHealthArr = [];
-    // let obstaclesLoveArr = [];
-    // let obstaclesLeisureArr = [];
-    // let winObj = null
-    let score = 0;
-
-    let draw3 = () => {
-        if (!runningGame) {
-            return;
-        }
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        counter++;
-        // drawing road image
-        //   let roadImg = new Image();
-        //   roadImg.src = "images/road.png";
-        //   ctx.drawImage(roadImg, 0, 0, canvas.width, canvas.height);
-        // creat obstacles
-        let ind
-        obstaclesArr.forEach((e, i) => {
-            if (player.crash(e)) {
-                // runningGame = false;
-                ind = i
-                score += 1
-                console.log("outPut: draw -> score", score)
-                // e.clear()
-            }
-            e.update();
-
-        });
-        if (ind) {
-            obstaclesArr.splice(ind, 1)
-
-        }
-        player.update();
-
-        if (counter % 120 === 0) {
-            let randomPox = Math.floor(Math.random() * (400 - 50) + 50);
-            let randomPox1 = Math.floor(Math.random() * (400 - 50) + 50);
-            if (randomPox <= 450 && randomPox1 <= 450 && randomPox != randomPox1) {
-                obstaclesArr.push(new obstacle(randomPox));
-                obstaclesArr.push(new obstacle(randomPox1));
-
-            }
-        }
-        // if (counter === 400) {
-        //     let randomPoxWin = Math.floor(Math.random() * (400 - 50) + 50);
-        //     if (randomPoxWin <= 450) {
-        //         winObj = new winObject(randomPoxWin)
-        //     }
-        // }
-        // if (winObj !== null) {
-        //     winObj.update()
-        //     if (player.crash(winObj)) {
-        //         main.startGame()
-
-        //     }
-        // }
-
-
-        window.requestAnimationFrame(draw3);
-    };
-    // Function for moving player right, left, up, down
-
-    document.onkeydown = function (e) {
-        switch (e.keyCode) {
-            case 37:
-                player.leftPressed();
-                break;
-            case 39:
-                player.rightPressed();
-                break;
-            case 38:
-                player.upPressed()
-                break;
-            case 40:
-                player.downPressed()
-                break;
-
-        }
-    };
-    // start game function
-    function startGame() {
-        draw3();
-    }
+let cheese = "lactose intolerant";
+document.getElementById("start-button").onclick = () => {
+  startGame();
 };
+// Getting the canvas from html page
+let ctx = document.getElementById("canvas").getContext("2d");
+// For ending the game
+let runningGame = true;
+// Create car objectfksdjf
+let player = {
+  x: 220,
+  y: 400,
+  width: 50,
+  height: 50,
+  img: new Image(),
+  rightPressed: function() {
+    if (this.x < 450 - this.width) {
+      this.x += 15;
+    }
+  },
+  leftPressed: function() {
+    if (this.x >= 50) {
+      this.x -= 15;
+    }
+  },
+  upPressed: function() {
+    if (this.y >= 50) {
+      this.y -= 15;
+    }
+  },
+  downPressed: function() {
+    if (this.y <= 400) {
+      this.y += 15;
+    }
+  },
+  update: function() {
+    // ctx.fillRect(this.x, this.y, this.width, this.height);
+    // this.img.src = "car.png";
+    // ctx.drawImage(roadImg, 0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "black";
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  },
+  left: function() {
+    return this.x;
+  },
+  top: function() {
+    return this.y;
+  },
+  right: function() {
+    return this.x + this.width;
+  },
+  bottom: function() {
+    return this.y + this.height;
+  },
+  crash: function(obstacle) {
+    return !(
+      this.bottom() < obstacle.top() ||
+      this.top() > obstacle.bottom() ||
+      this.right() < obstacle.left() ||
+      this.left() > obstacle.right()
+    );
+  }
+  // win: function (winObj) {
+  //     return (
+  //         this.bottom() < winObj.top() ||
+  //         this.top() > winObj.bottom() ||
+  //         this.right() < winObj.left() ||
+  //         this.left() > winObj.right()
+  //     );
+  // }
+};
+// Create obstacles
+class obstacle {
+  constructor(posX) {
+    this.x = posX;
+    this.y = 0;
+    this.width = 20;
+    this.height = 20;
+    this.color = "green";
+    this.speedY = 2;
+  }
+  update() {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+    // this.img.src = "car.png";
+    // ctx.drawImage(roadImg, 0, 0, canvas.width, canvas.height);
+    this.y += 2;
+  }
+  top() {
+    return this.y;
+  }
+  left() {
+    return this.x;
+  }
+  bottom() {
+    return this.y + this.height;
+  }
+  right() {
+    return this.x + this.width;
+  }
+  clear() {
+    let prevCoords = {};
+    // prevCoords.x = this.x;
+    // console.log("outPut: obstacle -> clear -> prevCoords.x", prevCoords.x)
+    // prevCoords.y = this.y - 2;
+    // console.log("outPut: obstacle -> clear -> prevCoords.y", prevCoords.y)
+    // lineWidth:fix
+    // ctx.clearRect(this.x, this.y + 2, this.width, this.height)
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
+}
+
+// class objectMoney extends object {
+//     constructor(posX) {
+//         super(posX)
+//         this.y = 0;
+//         this.width = 20;
+//         this.height = 20;
+//         this.color = 'green';
+//         this.speedY = 2;
+//         this.img = new Image()
+//     }
+// }
+// class objectHealth extends object {
+//     constructor(posX) {
+//         super(posX)
+//         this.y = 0;
+//         this.width = 20;
+//         this.height = 20;
+//         this.color = 'blue';
+//         this.speedY = 2;
+//         this.img = new Image()
+//     }
+// }
+// class objectLove extends object {
+//     constructor(posX) {
+//         super(posX)
+//         this.y = 0;
+//         this.width = 20;
+//         this.height = 20;
+//         this.color = 'red';
+//         this.speedY = 2;
+//         this.img = new Image()
+//     }
+// }
+// class objectLeisure extends object {
+//     constructor(posX) {
+//         super(posX)
+//         this.y = 0;
+//         this.width = 20;
+//         this.height = 20;
+//         this.color = 'yellow';
+//         this.speedY = 2;
+//         this.img = new Image()
+//     }
+// }
+
+// class winObject {
+//     constructor(posX) {
+//         this.x = posX;
+//         this.y = 0;
+//         this.width = 100;
+//         this.height = 50;
+//         this.color = 'blue';
+//         this.speedY = 2;
+//     }
+//     update() {
+//         ctx.fillStyle = this.color;
+//         ctx.fillRect(this.x, this.y, this.width, this.height);
+//         this.y += 2;
+//     }
+//     top() {
+//         return this.y - 8;
+//     }
+//     left() {
+//         return this.x - 8;
+//     }
+//     bottom() {
+//         return this.y + this.height - 8;
+//     }
+//     right() {
+//         return this.x + this.width - 8;
+//     }
+// }
+
+let counter = 0;
+let obstaclesArr = [];
+
+// let obstaclesMoneyArr = [];
+// let obstaclesHealthArr = [];
+// let obstaclesLoveArr = [];
+// let obstaclesLeisureArr = [];
+// let winObj = null
+let score = 0;
+
+let draw = () => {
+  if (!runningGame) {
+    return;
+  }
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  counter++;
+  // drawing road image
+  //   let roadImg = new Image();
+  //   roadImg.src = "images/road.png";
+  //   ctx.drawImage(roadImg, 0, 0, canvas.width, canvas.height);
+  // creat obstacles
+  let ind;
+  player.update();
+  console.log(obstaclesArr)
+  obstaclesArr.forEach((e, i) => {
+    if (player.crash(e)) {
+      // runningGame = false;
+      ind = i;
+      score += 1;
+      console.log("outPut: draw -> score", score);
+      console.log(ind)
+      // e.clear()
+    }
+    e.update();
+  });
+  if (ind !== undefined) {
+    obstaclesArr.splice(ind, 1); 
+  }
+  
+  
+  
+  
+
+  if (counter % 120 === 0) {
+    let randomPox = Math.floor(Math.random() * (400 - 50) + 50);
+    let randomPox1 = Math.floor(Math.random() * (400 - 50) + 50);
+    if (randomPox <= 450 && randomPox1 <= 450 && randomPox != randomPox1) {
+      //obstaclesArr.push(new obstacle(randomPox));
+      obstaclesArr.push(new obstacle(randomPox1));
+    }
+  }
+  // if (counter === 400) {
+  //     let randomPoxWin = Math.floor(Math.random() * (400 - 50) + 50);
+  //     if (randomPoxWin <= 450) {
+  //         winObj = new winObject(randomPoxWin)
+  //     }
+  // }
+  // if (winObj !== null) {
+  //     winObj.update()
+  //     if (player.crash(winObj)) {
+  //         main.startGame()
+
+  //     }
+  // }
+// if(score===1){
+//     itemMoneyArr[0].drawPickedItem()
+// // }
+// // if(score===2){
+//     itemMoneyArr[0].drawPickedItem()
+//     itemMoneyArr[1].drawPickedItem()
+// }
+// if(score===3){
+    // itemMoneyArr[0].drawPickedItem()
+    // itemMoneyArr[1].drawPickedItem()
+    // itemMoneyArr[2].drawPickedItem()
+  drawItemMoney(score)
+  drawItemLove(score)
+  drawItemHealth(score)
+  drawItemLeisure(score)
+// }
+
+    // itemMoneyArr[0].drawPickedItem()
+    // itemMoneyArr[1].drawPickedItem()
+    // itemMoneyArr[2].drawPickedItem()
+    // itemMoneyArr[3].drawPickedItem()
+  drawMiddleAndAxis()
+  //drawItemMoney()
+  // drawItemLove()
+  // drawItemHealth()
+  // drawItemLeisure()
+  window.requestAnimationFrame(draw);
+};
+// Function for moving player right, left, up, down
+
+document.onkeydown = function(e) {
+  switch (e.keyCode) {
+    case 37:
+      player.leftPressed();
+      break;
+    case 39:
+      player.rightPressed();
+      break;
+    case 38:
+      player.upPressed();
+      break;
+    case 40:
+      player.downPressed();
+      break;
+  }
+};
+// start game function
+function startGame() {
+  draw();
+  
+}
